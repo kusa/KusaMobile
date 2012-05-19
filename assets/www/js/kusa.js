b788b0e150df169bc726c6306570d8e4046a5fef
kusa <ondrej.tyrychtr@gmail.com>
2012-05-20 01:27:34 +0200

    wqqwwq

--- assets/www/js/kusa.js
+++ assets/www/js/kusa.js
@@ -8,7 +8,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
  }
 
  /** Connection **/
- function checkConnection() {
+ function checkConnection()
+ {
      var networkState = navigator.network.connection.type;
 
      var states = {};
@@ -26,4 +27,45 @@ document.addEventListener("deviceready", onDeviceReady, false);
     	 $(".connection span").attr("class", "none");
      else
     	 $(".connection span").attr("class", "");
+ }
+
+ /** Fitness **/
+ var fitnessDef = []; /*Pocet sekund pro kazdou fazy*/
+ var fitnessRepeat = false; /*Opakovani cyklu*/
+ 
+ $("#fitnessSave").live("vclick", function(){
+	 fitness.saveOption();
+ })
+ 
+  $("#fitnessStart").live("vclick", function(){
+	 fitness.start();
+ })
+ 
+
+ function fitness() 
+ {
+	 
+ }
+ 
+ fitness.start = function()
+ {
+	 fitness.test()
+ }
+ 
+ fitness.saveOption = function()
+ {
+	 fitnessDef = [];
+
+	 if (!isNaN($("#phase1").val()) && parseInt($("#phase1").val()) != 0) fitnessDef.push(parseInt($("#phase1").val()));
+	 if (!isNaN($("#phase2").val()) && parseInt($("#phase2").val()) != 0) fitnessDef.push(parseInt($("#phase2").val()));
+	 if (!isNaN($("#phase3").val()) && parseInt($("#phase3").val()) != 0) fitnessDef.push(parseInt($("#phase3").val()));
+	 if (!isNaN($("#phase4").val()) && parseInt($("#phase4").val()) != 0) fitnessDef.push(parseInt($("#phase4").val()));
+	 
+	 fitnessRepeat = ($("#repeat option:selected").eq(0).val() == "on"?true:false);
+	 $('.ui-dialog').dialog('close');
+ }
+ 
+ fitness.test = function()
+ {
+	 alert(fitnessDef[0])
  }
